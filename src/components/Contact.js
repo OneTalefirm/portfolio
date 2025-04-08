492980571560b23f7c03d0bbbf45feb7fb6ab61e
OneTalefirm <onetalefirm@gmail.com>
2025-04-08 18:00:41 +0530

    Initial backend

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,7 +27,7 @@ export const Contact = () => {
     e.preventDefault();
     setButtonText("Sending...");
     try {
-      const response = await fetch("http://localhost:5001/contact", {
+      const response = await fetch("https://backend-7jby.onrender.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
